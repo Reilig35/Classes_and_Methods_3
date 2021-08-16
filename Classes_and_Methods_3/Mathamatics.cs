9cfc5555780d4165288e97bdfb43743e33ea7048
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-16 11:37:53 +0100

    Add project files.

---
+++ Classes_and_Methods_3/Mathamatics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_and_Methods_3
+{
+    class Mathamatics
+    {
+        public void twoParameter(int num1, int num2)
+        {
+            int multiBy6 = num1 * 6;
+            Console.WriteLine($"Your first number {num1} times 6 = {multiBy6}");
+            Console.WriteLine($"Your second number is {num2} no math operation carried out.");
+            Console.WriteLine("\n\n\nPress ENTER to continue");
+            Console.Read();
+            return;
+        }
+    }
+}
---
+++ Classes_and_Methods_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Classes_and_Methods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter two numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Mathamatics myMethod = new Mathamatics();
+            myMethod.twoParameter(num1, num2);
+        }
+    }
+}
